da28dc3022ff46fd127c2ee277df60118f233cdb
querenjie <32668511+querenjie@users.noreply.github.com>
2018-01-31 16:18:11 +0800

    Update DeployDbscriptDetailsqlPO.java

--- src/main/java/com/myself/deployrequester/po/DeployDbscriptDetailsqlPO.java
+++ src/main/java/com/myself/deployrequester/po/DeployDbscriptDetailsqlPO.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.util.UUID;
 
 /**
-*
+*bbbbbbbbbbbbbbbbbb
 */
 public class DeployDbscriptDetailsqlPO {
     private String deploydbscriptdetailsqlid = UUID.randomUUID().toString();
