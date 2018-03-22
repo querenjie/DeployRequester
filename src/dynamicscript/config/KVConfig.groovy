a3bb0a98d2a615b7c49ea4c52c91a7ab6cdd9435
hengli <12345678>
2018-03-22 18:48:14 +0800

    新增生产配置

--- src/dynamicscript/config/KVConfig.groovy
+++ src/dynamicscript/config/KVConfig.groovy
@@ -515,14 +515,17 @@ class KVConfig extends AbstractConfig {
         /**********配置允许访问此发布申请系统的ip地址 (end) **************************************************/
 
 
-        //配置预发布环境template1数据库中的数据库信息
-        addDatabaseConfig("DBServer1","172.16.54.12", "template1", "postgres", "suneeedba", "5440","大米预发布服务器172.16.54.12:5440");//大米预发布
-        addDatabaseConfig("DBServer2","172.16.54.11", "template1", "postgres", "suneeedba", "5441","大米预发布服务器172.16.54.11:5441");//大米预发布
+        //配置预数据库中的数据库信息
+        addDatabaseConfig("DBServer1","172.16.54.12", "template1", "postgres", "suneeedba", "5440","大米预发布服务器172.16.54.12:5440","1");//大米预发布
+        addDatabaseConfig("DBServer2","172.16.54.11", "template1", "postgres", "suneeedba", "5441","大米预发布服务器172.16.54.11:5441","1");//大米预发布
 
-        addDatabaseConfig("DBServer3","172.16.41.14", "template1", "postgres", "suneeedba", "5432","宁家预发布服务器172.16.41.14:5432");//宁家预发布
-        addDatabaseConfig("DBServer4","172.16.36.66", "template1", "postgres", "suneeedba", "5432","宁家预发布服务器172.16.36.66:5432");//宁家预发布
+        addDatabaseConfig("DBServer3","172.16.41.14", "template1", "postgres", "suneeedba", "5432","宁家预发布服务器172.16.41.14:5432","1");//宁家预发布
+        addDatabaseConfig("DBServer4","172.16.36.66", "template1", "postgres", "suneeedba", "5432","宁家预发布服务器172.16.36.66:5432","1");//宁家预发布
 
-        addDatabaseConfig( "DBServer5","172.16.41.20", "template1", "postgres", "suneeedba", "5432","翌能预发布服务器172.16.41.20:5432");//翌能预发布
+        addDatabaseConfig( "DBServer5","172.16.41.20", "template1", "postgres", "suneeedba", "5432","翌能预发布服务器172.16.41.20:5432","1");//翌能预发布
+
+        addDatabaseConfig( "DBServer6","172.16.41.26", "template1", "postgres", "suneeedba", "5433","翌能生产服务器172.16.41.26:5433","2");//翌能生产
+        addDatabaseConfig( "DBServer7","172.16.41.24", "template1", "postgres", "suneeedba", "5433","翌能生产服务器172.16.41.24:5433","2");//翌能生产
         /**********************配置预发布环境据库信息(end)**************************************************/
     }
 }
